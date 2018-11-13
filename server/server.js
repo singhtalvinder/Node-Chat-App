e1ad1aa7d9154596600ce6b2fe753a47a974abca
unknown <talvindersingh.ca@gmail.com>
2018-11-13 14:07:25 -0500

    Added connect and disconnect handlers for sockets app

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const path = require('path'); // NO need to install it as its a built-in module.
+const http = require('http'); // No need to install it as its a built-in module.
+const path = require('path'); // No need to install it as its a built-in module.
+const socketIO = require('socket.io');
 
 // path helps in handling directories in a very effecient manner.
 const publicPath = path.join(__dirname, "../public");
@@ -7,9 +9,24 @@ const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 // setup public middleware folder.
 app.use(express.static(publicPath));
-app.listen(port, () =>
+
+// register for events.
+// Listen for individual connection on a socket.
+io.on('connection', (socket) => {
+    console.log('New user connected !! ');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected from server !! ');
+    });
+});
+
+
+server.listen(port, () =>
 {
     console.log(`Server started at port ${port} `);
 });
