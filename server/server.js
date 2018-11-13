21b347ca2eb77ec30945f70db2a4300a83dfc1e7
unknown <talvindersingh.ca@gmail.com>
2018-11-12 23:21:31 -0500

    Initial setup for Chat app

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path'); // NO need to install it as its a built-in module.
+
+// path helps in handling directories in a very effecient manner.
+const publicPath = path.join(__dirname, "../public");
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+// setup public middleware folder.
+app.use(express.static(publicPath));
+app.listen(3000, () =>
+{
+    console.log(`Server started at port ${port} `);
+});
+
