36d3b4d7fd354039f2d42089a17af5b346487364
unknown <talvindersingh.ca@gmail.com>
2018-11-13 22:47:30 -0500

    Added Form for messages using jQuery and neat display.

--- public/js/index.js
+++ public/js/index.js
@@ -11,7 +11,34 @@
 
     socket.on('newMessage', function(message){
         console.log('newMessage !! ', message);
+        var li = jQuery('<li></li>');
+        li.text(`${message.from}: ${message.text}`);
+        jQuery('#messages').append(li);
     });
+
+    /*
+     //Not needed
+     socket.emit('createMessage', {
+        from: 'Mark',
+        text: 'Hello there!!'
+    }, function(data){ // data is what is returned from the callback of the 
+        //socket.on('createMessage',  method in the server.
+        console.log('Got the message', data);
+    });
+    */
+
+    // using jQuery to avoid the page refresh and 
+    // not to send the form data as appended content to the url.
+    jQuery('#message-form').on('submit', function(e) {
+        e.preventDefault();
+        socket.emit('createMessage', {
+            from: 'User',
+            text: jQuery('[name=message]').val()
+        }, function() {
+            // acknowledgement func.
+        });
+    });
+
 
     /*
     // Test code:
--- server/server.js
+++ server/server.js
@@ -31,13 +31,13 @@ io.on('connection', (socket) => {
         console.log('Client disconnected from server !! ');
     });
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('Created Message:', message);
        
         // Emit message to every connected user.
         // but not to itself.
         io.emit('newMessage', generateMessage(message.from, message.text));
-        
+        callback('This is from the server');
         /*
         socket.broadcast.emit('newMessage',  {
             from: message.from,
